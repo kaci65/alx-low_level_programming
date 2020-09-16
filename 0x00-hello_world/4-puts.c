66776761d3f6b461567eca515eb7db291583470a
kaci65 <wanjikukarugi@gmail.com>
2020-09-16 16:11:41 +0300

    Add file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+* print_string - print a string
+*/
+#include <stdio.h>
+#include <string.h>
+
+print_string(void)
+{
+char string;
+
+strcpy(str, "Programming is like building a multilingual puzzle\n");
+puts(string);
+return (0);
+}
