93a16ac728e397459adf1b7962ff98d7f2412f6c
kaci65 <wanjikukarugi@gmail.com>
2020-09-17 23:47:13 +0300

    Add file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - alphabet in lowercase and uppercase
+* Return: 0 if successful
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+
+	return (0);
+}
