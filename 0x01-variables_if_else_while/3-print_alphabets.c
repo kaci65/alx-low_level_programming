260c2472c9c3448eca6990bf552e5a9a61493f2c
kaci65 <wanjikukarugi@gmail.com>
2020-09-17 23:57:21 +0300

    Edit file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,12 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-		putchar(ch);
-		putchar('\n');
-	}
+	/* print uppercase letters */
+		for (ch = 'A'; ch <= 'Z'; ch++)
+		{
+			putchar(ch);
+			putchar('\n');
+		}
 
 	return (0);
 }
