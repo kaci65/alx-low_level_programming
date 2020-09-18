e5ac412f49d0b26064edfbc4c12aac2f674c2e43
kaci65 <wanjikukarugi@gmail.com>
2020-09-18 03:06:26 +0300

    Fix file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 		putchar(letter);
 	}
 	/* print numbers */
-		for (number = 0; number <= 9; number++)
+		for (number = '0'; number <= '9'; number++)
 		{
 			putchar(number);
 		}
