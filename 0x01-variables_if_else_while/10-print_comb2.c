4daf2bc54e5cc109b80ee7ab438584abfca8d33c
kaci65 <wanjikukarugi@gmail.com>
2020-09-18 04:27:10 +0300

    Add file

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - 0 to 99 in double digits, seperated by commas and spaces
+* Return: 0 if successful
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 99; number++)
+	{
+		putchar("%.2d", number);
+	}
+	/* print uppercase letters */
+		if (number != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+	return (0);
+}
