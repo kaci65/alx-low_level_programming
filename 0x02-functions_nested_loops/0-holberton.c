66ce63f00aeddd994d2f0875fae7edcae3610d21
kaci65 <wanjikukarugi@gmail.com>
2020-09-22 21:11:32 +0300

    Add file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*main - print a word string
+*Return: 0 if successful
+*/
+int main(void)
+{
+	int i;
+	char str = "Holberton";
+
+	for (i = 1; i < 9; i++)
+		_putchar(str[i]);
+
+	_putchar(“\n”);
+
+	return (0);
+}
