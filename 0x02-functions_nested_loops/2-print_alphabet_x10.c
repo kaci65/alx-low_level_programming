33def3cbab670acc0f81168dc1b90f4d2132d652
kaci65 <wanjikukarugi@gmail.com>
2020-09-22 22:02:18 +0300

    Add file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* print_alphabet - alphabet in lowercase ten times
+* Return: 0 if successful
+*/
+void print_alphabet(void)
+{
+	int i;
+	int ch;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
