23fb8b1b35d6e199cb54aa201bcaadc5a74e38eb
kaci65 <wanjikukarugi@gmail.com>
2020-09-23 21:23:08 +0300

    Edit file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,39 +5,22 @@
 */
 void jack_bauer(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int x = 0;
+	int j = 0;
 
-	for (a = 0; a <= 2; a++)
+	while (x <= 23)
 	{
-		for (b = 0; b <= 3; b++)
+		j = 0;
+		while (j <= 59)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a < 2)
-					{
-						_putchar('0' + a);
-						_putchar('0' + b);
-						_putchar(':');
-						_putchar('0' + c);
-						_putchar('0' + d);
-						_putchar('\n');
-					}
-					else if (a == 2 && b <= 3)
-					{
-						_putchar('0' + a);
-						_putchar('0' + b);
-						_putchar(':');
-						_putchar('0' + c);
-						_putchar('0' + d);
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j += 1;
 		}
+		x += 1;
 	}
 }
