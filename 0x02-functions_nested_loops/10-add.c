32816eb42b1a168a6d57c77d37821bb8d3936998
kaci65 <wanjikukarugi@gmail.com>
2020-09-23 22:22:20 +0300

    Edit file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,11 @@
 #include "holberton.h"
 /**
  *add - adds two integers and returns the result
- *@num1: integer one
- *@num2: integer two
- *Return: integer of sum
+ *@i: integer one
+ *@j: integer two
+ *Return: result of addition
  */
-int add(int num1, int num2)
+int add(int i, int j)
 {
-	int sum;
-
-	sum = num1 + num2;
-	return (sum);
+	return (i + j);
 }
