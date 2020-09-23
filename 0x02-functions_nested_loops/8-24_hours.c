dcf1a3c8689fffc5cfc3af0e9e26d9f1f9c201dd
kaci65 <wanjikukarugi@gmail.com>
2020-09-23 20:59:34 +0300

    Edit file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
 *jack_bauer - print every minute starting from 00:00 to 23:59
@@ -8,24 +7,24 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
+	int c;
 	int d;
-	int e;
 
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 3; b++)
 		{
-			for (d = 0; d <= 5; d++)
+			for (c = 0; c <= 5; c++)
 			{
-				for (e = 0; e <= 9; e++)
+				for (d = 0; d <= 9; d++)
 				{
 					if (a < 2)
 					{
 						_putchar('0' + a);
 						_putchar('0' + b);
 						_putchar(':');
+						_putchar('0' + c);
 						_putchar('0' + d);
-						_putchar('0' + e);
 						_putchar('\n');
 					}
 					else if (a == 2 && b <= 3)
@@ -33,8 +32,8 @@ void jack_bauer(void)
 						_putchar('0' + a);
 						_putchar('0' + b);
 						_putchar(':');
+						_putchar('0' + c);
 						_putchar('0' + d);
-						_putchar('0' + e);
 						_putchar('\n');
 					}
 				}
