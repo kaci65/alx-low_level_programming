e69cb9072ef1106f47df95f4f8d6503c8fe03064
kaci65 <wanjikukarugi@gmail.com>
2020-09-23 20:34:29 +0300

    Edit file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * _abs - compute the absolute value of an integer
- * @j: integer to check absolute value
- Return: positive integer
+ *_abs - compute the absolute value of an integer
+ *@j: integer to check absolute value
+ *Return: positive integer
  */
 int _abs(int j)
 {
