a5e3b65b4b330254d6796a3b798cf5f0aa63cc33
kaci65 <wanjikukarugi@gmail.com>
2020-09-23 20:20:44 +0300

    Edit file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 /**
 * add - adds two integers and returns the result
-* @x: an integer to add to y
-* @y: an integer to add to x
-* Return: void
+* @x: an integer to add
+* @y: an integer to add
+* Return: integer value
 */
 int add(int x, int y)
 {
-	return (x + y);
+	int sum;
+
+	sum = x + y;
+	return (sum);
 }
