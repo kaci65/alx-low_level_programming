2ce5b9ea8ab0b9de78ba7c1afd3037d33b7622b3
kaci65 <wanjikukarugi@gmail.com>
2020-09-24 15:57:11 +0300

    Attempt to edit file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,12 @@ void print_square(int size)
 {
 	int i;
 	int j;
-	
+
 	if (size > 0)
 	{
-		for (i = 1; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j < i; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar(35);
 			}
