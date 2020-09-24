bf70c5e3605ac9e32a547b8633c54bfa3ca87ff8
kaci65 <wanjikukarugi@gmail.com>
2020-09-24 16:26:56 +0300

    Attempt to edit file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("%s", "FizzBuzz");
 		}
-		if (i != 100)
+		else if (i != 100)
 		{
 			putchar(32);
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("%s", "Buzz");
 		}
 		else
 		{
