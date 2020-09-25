6680a1c189363a827892c09bb21a494a78ad1a92
kaci65 <wanjikukarugi@gmail.com>
2020-09-25 10:07:50 +0300

    Attempt to fix file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,16 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j > 10)
+			{
+				_putchar(j / 10);
+				_putchar(j % 10);
+			}
 		}
 		_putchar('\n');
-		i++;
 	}
-	_putchar('\n');
 }
