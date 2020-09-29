433079856c69cf5df499827baa121f22ea412879
kaci65 <wanjikukarugi@gmail.com>
2020-09-29 12:29:38 +0300

    Attempt to fix file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,14 @@
 
 /**
  * _puts - print a string
- * @str: string to be printed
+ * @str: pointer of string to be evaluated
  * Return: void
  */
 
 void _puts(char *str)
-{	
+{
 	int j = 0;
-	
+
 	while (str[j] != '\0')
 	{
 		_putchar(str[j]);
