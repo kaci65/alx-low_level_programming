5d818f9894297ebd034d569a480ff15f8ed4581e
kaci65 <wanjikukarugi@gmail.com>
2020-10-01 22:30:51 +0300

    Add file

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(i);
+	}
+}
+
