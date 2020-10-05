4c19f231a6f8626872ef89e7ad50e612f81fe204
kaci65 <wanjikukarugi@gmail.com>
2020-10-05 19:05:36 +0300

    Add file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,21 +4,30 @@
  * _strspn - gets the length of a prefix substring
  * @s: s segment containing bytes in @accept
  * @accept: bytes to check
- * Return: number of bytes that are coincidental
+ * Return: The number of bytes in @s which
+ *         consist only of bytes from @accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int c;
-	int position;
-	int length;
+	unsigned int c; /** bytes */
+	int index;
 
 	c = 0;
-	while (c < length)
+	while (s++)
 	{
-		accept[c] = s[position + c - length];
-		c++;
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				c++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (c);
+			}
+		}
 	}
-	accept[c] = '\0';
 	return (c);
 
 
