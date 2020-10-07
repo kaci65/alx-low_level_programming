c3415875bfb0f8fdc8028e0241a54b32767dda1f
kaci65 <wanjikukarugi@gmail.com>
2020-10-07 19:04:16 +0300

    Add file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -5,5 +5,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_value(int a, int b);
 int is_prime_number(int n);
 int is_palindrome(char *s);
