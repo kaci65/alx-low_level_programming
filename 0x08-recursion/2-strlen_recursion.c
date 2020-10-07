a6fa5839ace37f2a3dc22fc274ae36fb8b40c3ca
kaci65 <wanjikukarugi@gmail.com>
2020-10-07 17:13:40 +0300

    Add files

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - prints a string in reverse
+ * @s: length of string @s to be found
+ * Return: Integer
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		length++;
+		length = length + _strlen_recursion(s + 1);
+	}
+	return (length);
+}
