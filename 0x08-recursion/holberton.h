3fb46c8f23ad83dcdf4f3ad229a44a0cbaefe055
kaci65 <wanjikukarugi@gmail.com>
2020-10-07 20:20:38 +0300

    Fix file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,4 +7,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_value(int a, int b);
 int is_prime_number(int n);
+int find_prime(int num1, int i);
 int is_palindrome(char *s);
