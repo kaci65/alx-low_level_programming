55e9f8d1344ce31fbc2e062ed662754e937c279b
kaci65 <wanjikukarugi@gmail.com>
2020-10-15 22:41:41 +0300

    Edit file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,21 +17,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str_concat;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	len_s1 = strlen(s1);
 	len_s2 = strlen(s2);
 
 	if (n >= len_s2)
-	{
 		n = len_s2;
-	}
 
 	str_concat = malloc(sizeof(char) * (n + len_s1 + 1));
 
