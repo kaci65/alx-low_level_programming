f1c873d021c18b6f1bfaed10dcf68ef610723354
kaci65 <wanjikukarugi@gmail.com>
2020-10-22 21:36:43 +0300

    Fix files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function to perform the calculation
@@ -22,12 +24,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
 	while (i < 5)
 	{
-		if (*s == ops[i].op)
+		if (strcmp(ops[i].op, s) == 0)
 		{
-
+			return (ops[i].f);
 		}
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
+
+/**
+ * main - prints mathematical functions
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int num1, num2;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	printf("%d\n", op_func(num1, num2));
+
+	return (0);
+}
