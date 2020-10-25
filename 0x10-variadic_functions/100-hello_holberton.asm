2c7eaa49439267608624bb98790121eb8c145be0
kaci65 <wanjikukarugi@gmail.com>
2020-10-25 23:26:44 +0300

    Add file

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,20 @@
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .rodata
+	msg: db "Hello, Holberton", 0xa
+	len: equ $ - msg
