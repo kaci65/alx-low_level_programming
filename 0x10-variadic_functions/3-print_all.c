37748cdd20607d616b51f96de0940ae68915531c
kaci65 <wanjikukarugi@gmail.com>
2020-10-26 10:24:50 +0300

    Edit file to not use switch statement

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,59 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - function that prints character
+ * @argptr: argument list pointer to character to be printed
+ */
+void print_char(va_list argptr)
+{
+	char character;
+
+	character = va_arg(argptr, int);
+	printf("%c", character);
+}
+
+/**
+ * print_int - function that prints integer
+ * @argptr: argument list pointer to integer to be printed
+ */
+void print_int(va_list argptr)
+{
+	int num_i;
+
+	num_i = va_arg(argptr, int);
+	printf("%i", num_i);
+}
+
+/**
+ * print_float - function that prints a float
+ * @argptr: argument list pointer to float to be printed
+ */
+void print_float(va_list argptr)
+{
+	float num_f;
+
+	num_f = va_arg(argptr, double);
+	printf("%f", num_f);
+}
+
+/**
+ * print_string - function that prints string
+ * @argptr: argument list pointer to string to be printed
+ */
+void print_string(va_list argptr)
+{
+	char *str;
+
+	str = va_arg(argptr, char*);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", str);
+}
+
+/**
  * print_all - function that prints anything
  * @format: list of types of arguments passed to the function
  * @...: number of arguments
@@ -10,35 +63,42 @@
  */
 void print_all(const char * const format, ...)
 {
-	int num, character;
-	double num_D;
-	char *str;
-	char const *print_loop;
+	va_list arguments;
+	int index, arg_len;
+	char *separator = "";
 
-	va_list args;
+	prints_t fmt_list[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 
-	va_start(args, format);
+	va_start(arguments, format);
 
-	for (print_loop = format; *print_loop != '\0'; print_loop++)
+	index = 0;
+	while (format && (*(format + index)))
+	/**
+	 * skip to next stack by adding index, skipping
+	 * to next data type in fmt_list
+	 */
 	{
-		switch (*print_loop)
+		arg_len = 0;
+		while (arg_len < 4 && (*(format + index) != *(fmt_list[arg_len].notation)))
 		{
-			case 'i':
-				num = va_arg(args, int);
-				printf("%i, ", num);
-				break;
-			case 'c':
-				character = va_arg(args, int);
-				printf("%c, ", character);
-				break;
-			case 'f':
-				num_D = va_arg(args, double);
-				printf("%f, ", num_D);
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				printf("%s", str);
-				break;
+			arg_len++;
 		}
+
+		if (arg_len < 4)
+		{
+			printf("%s", separator);
+			fmt_list[arg_len].print_dType(arguments);
+			separator = ", ";
+		}
+		index++;
 	}
+	printf("\n");
+	va_end(arguments);
+
 }
+
