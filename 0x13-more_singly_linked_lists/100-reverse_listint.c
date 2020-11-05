81a492fc5ac864614c2a16dd31852bbedd805708
kaci65 <wanjikukarugi@gmail.com>
2020-11-05 23:09:39 +0300

    Add file

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,12 +8,12 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev_node, *next_node;
-	
+
 	if (!*head)
 	{
 		return (NULL);
 	}
-	
+
 	prev_node = NULL;
 
 	while (*head != NULL)
@@ -22,8 +22,8 @@ listint_t *reverse_listint(listint_t **head)
 		(*head)->next = prev_node;
 		prev_node = *head;
 		*head = next_node;
-	}	
+	}
 	*head = prev_node;
-	
+
 	return (*head);
 }
