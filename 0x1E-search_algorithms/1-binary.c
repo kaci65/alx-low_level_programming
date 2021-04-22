4fcdddea456beb6a8d9cd8dfdf7c8ebda13de373
kaci65 <wanjikukarugi@gmail.com>
2021-04-22 22:38:31 +0300

    Fix file: combine the two functions, use 'else if' on line 48

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,35 +2,6 @@
 #include <stdio.h>
 
 /**
- * print_array - print array after every iteration
- * @array: pointer to the first element of the array
- * @start: index to start printing array
- * @end: last index to stop printing at
- */
-void print_array(int *array, size_t start, size_t end)
-{
-	size_t n = start;
-	/**
-	 * n should always be equal to the element in the first index
-	 * after every iteration (if the value is not found)
-	 */
-
-	printf("Searching in array: ");
-	while (n <= end)
-	{
-		printf("%d", array[n]);
-		if (n != end)
-		{
-			printf(", ");
-		}
-		else
-		{
-			printf("\n");
-		}
-		n++;
-	}
-}
-/**
  * binary_search - searches for a value in sorted array of integers
  *                 using Binary search algorithm
  * @array: pointer to the first element of the array to search in
@@ -43,32 +14,41 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t start = 0;
 	size_t end = size - 1;
-	size_t midpoint;
+	size_t midpoint, n;
 
-	if (array)
+	if (!array)
 	{
-		while (start <= end)
+		return (-1);
+	}
+	while (start <= end)
+	{
+		printf("Searching in array: ");
+		for (n = start; n <= end; n++)
 		{
-			print_array(array, start, end);
-			midpoint = (start + end) / 2;
-
-			if (array[midpoint] > value)
+			printf("%d", array[n]);
+			if (n != end)
 			{
-				end = midpoint - 1;
-			}
-			if (array[midpoint] < value)
-			{
-				start = midpoint + 1;
+				printf(", ");
 			}
 			else
 			{
-				return (midpoint);
+				printf("\n");
 			}
 		}
-	}
-	else
-	{
-		return (-1);
+		midpoint = (start + end) / 2;
+
+		if (array[midpoint] > value)
+		{
+			end = midpoint - 1;
+		}
+		if (array[midpoint] < value)
+		{
+			start = midpoint + 1;
+		}
+		else if (array[midpoint] == value)
+		{
+			return (midpoint);
+		}
 	}
 	return (-1);
 }
